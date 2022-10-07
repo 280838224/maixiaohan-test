e0b7bae1aed8a7ef15cf5fad982763bbf4e46bef
maihan <280838224@qq.com>
2022-10-07 14:31:59 +0800

    111

---
+++ src/demo.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Maixiaohan\Test;
+
+/**
+ * @Author: maixiaohan
+ * @Date:   2022-10-07 14:29:47
+ * @Last Modified by:   maixiaohan
+ * @Last Modified time: 2022-10-07 14:29:47
+ */
+
+class Demo 
+{
+	
+	function myTest()
+	{
+		return 'success!!!';
+	}
+}
